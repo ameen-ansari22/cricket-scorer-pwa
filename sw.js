63e2a8e2fd8364e94f879ad976e1a78201149f29
Mohammed Ameen Ansari <ameanansari3912@gmail.com>
2025-08-30 19:29:32 +0530

    Added PWA support with manifest and service worker

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "cricket-scorer-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/script.js",
+  "/manifest.json"
+];
+
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
