d9924c08349438fe623b841d48714e1ace6df556
Mohammed Ameen Ansari <ameanansari3912@gmail.com>
2025-08-30 19:47:42 +0530

    inatallable

--- script.js
+++ script.js
@@ -718,7 +718,8 @@ function newMatch() {
 }
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js")
+    navigator.serviceWorker 
+      .register("/sw.js")
       .then(reg => console.log("Service Worker registered:", reg))
       .catch(err => console.error("Service Worker failed:", err));
   });
